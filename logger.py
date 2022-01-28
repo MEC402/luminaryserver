4e6e8baaf9e3a2c86f89e2bfe90db4a6f32863b9
Kenny-Miller <kennymilarhol@gmail.com>
2022-01-28 16:37:53 -0700

    Converted logger into a function

--- logger.py
+++ logger.py
@@ -1,16 +1,14 @@
 import logging
 
-class Logger():
-  @classmethod
-  def create_logger(cls, name) -> logging.Logger:
-    """
-    Creates a generic logger.
-    """
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('[%(asctime)s] %(message)s')
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-    return logger
+def create_logger(name) -> logging.Logger:
+  """
+  Creates a generic logger.
+  """
+  logger = logging.getLogger(name)
+  logger.setLevel(logging.DEBUG)
+  ch = logging.StreamHandler()
+  ch.setLevel(logging.DEBUG)
+  formatter = logging.Formatter('[%(asctime)s] %(message)s')
+  ch.setFormatter(formatter)
+  logger.addHandler(ch)
+  return logger
