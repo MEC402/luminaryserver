a672047814a7219fefaa74c88244197188e8ef7d
Kenny-Miller <kennymilarhol@gmail.com>
2022-01-28 16:39:23 -0700

    Updated click/drag logic

--- wallclient.py
+++ wallclient.py
@@ -1,27 +1,57 @@
 import socket
-from logger import Logger
-
+import pyautogui
+from logger import create_logger
+from pynput.mouse import Controller
 
 class WallClient():
   def __init__(self,ip,port) -> None:
-    self.logger = Logger.create_logger(__name__)
+    self.logger = create_logger(__name__)
     self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     self.sock.bind((ip,port))
+    self.mouse = Controller()
+    self.prev_pos = (None, None)
     self.running = False
   
   def start(self) -> None:
+    self.logger.debug('Starting Wall Client')
     self.running = True
     while self.running:
       (data, addr) = self.sock.recvfrom(1024)
-      msg = data.decode('utf-8')
-      self.logger.debug(f"[Message]: {msg} from {addr}")
-      match msg:
+      (action, msg) = data.decode('utf-8').split(":")
+      self.logger.debug(f"[Message]:{action} {msg} from {addr}")
+      match action:
         case 'exit': self.running = False
-        case _: self.handle(msg)
+        case 'click': self.click(msg)
+        case 'move': self.move(msg)
   
-  def handle(self, msg) -> None:
-    pass
-  
+  def click(self, msg) -> None:
+    (x, y, pressed) = msg.split(" ")
+    (s_x, s_y) = self.scale_coordinates(int(x),int(y))
+
+    # (s_x, s_y) = self.scale_coordinates(int(x),int(y))
+    # if pressed == True:
+    #   pass
+    #   pyautogui.moveTo(x=s_x,y=s_y)
+    # else:
+    #   pyautogui.dragTo(x=s_x,y=s_y, button='left')
+    if pressed == True:
+      pyautogui.leftClick(x=s_x, y=s_y)
+      self.prev_pos = (s_x, s_y)
+    else:
+      (prev_x, prev_y) = self.prev_pos
+      pyautogui.moveTo(x=prev_x, y=prev_y)
+      pyautogui.dragTo(x=s_x, y=s_y, button='left')
+
+  def move(self, msg) -> None:
+    (x, y) = msg.split(" ")
+    (s_x, s_y) = self.scale_coordinates(int(x),int(y))
+    pyautogui.moveTo(x=s_x,y=s_y)    
+
+  def scale_coordinates(self,x,y):
+    # Do math here
+
+    return (x,y)
+
 if __name__ == "__main__":
   hostname = socket.gethostname()
   listener_ip = socket.gethostbyname(hostname)
