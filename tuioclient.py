d6c1751dbb4e18ad212d13ca000054e9be7d76d7
stevencutchin <stevencutchin@boisestate.edu>
2022-02-12 11:20:08 -0700

    updated drag working on wall1

--- tuioclient.py
+++ tuioclient.py
@@ -1,3 +1,4 @@
+from math import floor
 from pythontuio import TuioClient
 from pythontuio import Cursor
 from pythontuio import TuioListener
@@ -8,7 +9,7 @@ import time
 class MyListener(TuioListener):
  
     def __init__(self,apanel):
-        self.timer = Timer(2.0,self.timeout)
+        self.timer = Timer(.125,self.timeout)
         self.mouse = Controller()
         self.started = 0
         self.lasttime = 0
@@ -28,27 +29,27 @@ class MyListener(TuioListener):
         (_, _, x, y, _, _, _) = params
 
         # xscale factor - these are all guesses
-        xscale = 1400
-        yscale = 1024
-        yoffset = 1024
-        dragtime = 10  # this is a wild guess in nanoseconds
+        xscale = 680.5
+        yscale = 1234
+        yoffset = -344
+        dragtime = 1  # this is a wild guess in nanoseconds
 
         #calculate the mouse position
         #we assume that x,y are values in the range of 0 to 1
-        px = x*xscale + (self.panel-1) * xscale
+        px = floor(x*xscale + (self.panel-1) * xscale)
         py = y*yscale + yoffset
-        #self.mouse.position = (px,py)
+        self.mouse.position = (px,py)
 
         if self.started == 0:
             self.timer.start()
             self.started = 1
-            #self.mouse.press(Button.left) # put this in when ready
+            self.mouse.press(Button.left) # put this in when ready
 
         #handle drag event -delay the timeout with every press
         currenttime = time.time_ns()
         if ((currenttime - self.lasttime) < dragtime):
            self.timer.cancel()
-           self.timer = Timer(2,self.timeout)
+           self.timer = Timer(.125,self.timeout)
            self.timer.start()
 
 
@@ -56,23 +57,23 @@ class MyListener(TuioListener):
        
  
     def timeout(self):
-        #self.mouse.release(Button.left)
-        self.timer = Timer(2, self.timeout)
+        self.mouse.release(Button.left)
+        self.timer = Timer(.125, self.timeout)
         self.started = 0
         self.lasttime = 0
-        for val in range(25):
-            print('timeout')
  
  
      
- 
-#panel numbers range from 1 to 17 
-# we will need a client for every individual panel
-#client = TuioClient(("10.31.11.138",3003))
-client = TuioClient(("localhost",3007))
-t = Thread(target=client.start)
-listener = MyListener(7)
-client.add_listener(listener)
- 
-t.start()
+if __name__ == "__main__":
+    #panel numbers range from 1 to 17 
+    # we will need a client for every individual panel
+
+    # If pulling be sure to modify tuioclient/listener to disable print statements
+    for i in range(1,7):
+        client = TuioClient(("10.31.11.138",3000+i))
+        #client = TuioClient(("localhost",3003))
+        t = Thread(target=client.start)
+        listener = MyListener(i)
+        client.add_listener(listener)        
+        t.start()
 
